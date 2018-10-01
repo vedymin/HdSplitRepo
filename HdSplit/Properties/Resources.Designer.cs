a4d53653ed3d24ad24ff51bc9f06b6a10bdd846b
Ja <michalskimlm@gmail.com>
2018-10-01 19:54:02 +0200

    Change namespace

--- HdSplit/Properties/Resources.Designer.cs
+++ HdSplit/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SplitHD.Properties {
+namespace HdSplit.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SplitHD.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SplitHD.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HdSplit.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- HdSplit/Properties/Settings.Designer.cs
+++ HdSplit/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SplitHD.Properties {
+namespace HdSplit.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
